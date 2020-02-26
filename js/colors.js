70b4f2f84c76e2b4a9890b3449ee2f58457d51e6
Mitchel Sandidge <masandidge12@gmail.com>
2020-02-26 14:45:42 -0600

    added event listeners to all JS  files

--- js/colors.js
+++ js/colors.js
@@ -4,7 +4,23 @@
 var colors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Pink', 'White', 'Black', 'Brown', 'Azure', 'Ivory', 'Teal', 'Silver', 'Navy blue', 'Pea green', 'Gray', 'Orange', 'Maroon', 'Charcoal', 'Aquamarine', 'Coral', 'Fuchsia', 'Wheat', 'Lime', 'Crimson', 'Khaki', 'Hot pink', 'Magenta', 'Plum', 'Olive', 'Cyan'];
 
 // Generates a random number to pull arary index from
-var randomNumber = Math.floor(Math.random() * (colors.length-1));
+var randomColor = Math.floor(Math.random() * (colors.length-1));
 
 // Display the lucky color to HTML doc
-document.getElementById('color').innerHTML = "Your lucky color is: " + colors[randomNumber];
+document.getElementById('color').innerHTML = "Your lucky color is: " + colors[randomColor];
+
+
+
+cookiePic.addEventListener('click', function(event) {
+
+    randomColor.style.display = 'inherit';
+
+    // Hide default instructions
+    var defaultInstructions = document.getElementById('default-instructions');
+    defaultInstructions.style.display = 'none';
+
+    // Display repeat instructions
+    var repeatInstructions = document.getElementById('repeat-instructions');
+    repeatInstructions.style.display = 'inherit';
+
+}, false);
